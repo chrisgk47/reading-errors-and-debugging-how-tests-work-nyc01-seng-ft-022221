7e5f62d9880b8858c1d35c8e654ff2215b3e7b2c
Christopher Kim <chris.kim1990@gmail.com>
2021-02-05 20:46:42 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
-first_number = 10.to_i
-second_number = 5.to_i
+first_number = 10
+second_number = 5
 add = first_number + second_number
-difference = first number - second_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
 
